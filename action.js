d7c7629eabfc1ccbc72747bc5a63cbecbb95bac9
lycan79 <rickyriquelme79@gmail.com>
2022-12-31 02:22:00 -0300

    cv-ticmas 1.0 finalizado

--- action.js
+++ action.js
@@ -1,22 +1,34 @@
+
 function myFunc()  {
-	var now = new Date();
+    var now = new Date();
 	var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-	document.getElementById('horaactual').innerHTML= time;
+	
+    document.getElementById('horaactual').innerHTML= time;
+
 }
+
 myFunc();
 setInterval(myFunc, 1000);
 
-/**************************************/
 
-function colorRojo(){    
-    experiencia.style.background="red";
-}
-function colorVerde(){    
-    estudios.style.background="green";
-}
-function colorAzul(){    
-    habilidades.style.background="blue";
-}
+function mostrarSaludo(){
+    var ahora=new Date(); 
+    var hora=ahora.getHours();
+
+
+    if (hora>=6 && hora<13) {
+        texto="Buenos días";  
+    } else if (hora>=13 && hora<21) { 
+        texto="Buenas tardes";
+    } else { 
+        texto="Buenas noches";
+    }
+   document.getElementById('saludo').innerHTML = texto;
+   
+} 
+
+mostrarSaludo();
+
 //************************************** */
 function mensaje(){
 document.getElementById('gracias').innerHTML= "gracias! Feliz año nuevo";
@@ -24,4 +36,17 @@ document.getElementById('gracias').innerHTML= "gracias! Feliz año nuevo";
 
 function cambiarImagenJS(){
 	document.getElementById("fotocv").src="img/sonrisa.jpeg";
-  }
+}
+/**********************************************/   
+  
+function colorRojo(){    
+    experiencia.style.background="deeppink";
+}
+function colorVerde(){    
+    estudios.style.background="yellow";
+}
+function colorAzul(){    
+    habilidades.style.background="coral";
+}
+
+/****************************************** */
